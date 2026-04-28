d912a5c59458039838ff230026a8854a5d35f5a1
Yuga Bharathi Jaisankar <yuga.bharathijai2106@gmail.com>
2026-04-28 17:15:19 +0530

    fix: smart UI packaging and manifest BOM strip with version bump

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 name 'spz-speedometer'
 description 'Premium Racing Speedometer with spz-physics integration'
-version '1.1.1'
+version '1.1.2'
 author 'SPiceZ-Core'
 
 ui_page 'ui/dist/index.html'
